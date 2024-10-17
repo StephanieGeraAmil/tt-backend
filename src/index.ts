b9a314481ba6a6c556763c48f402035bda940fd0
StephanieGeraAmil <stephanie.gera.amil@gmail.com>
2024-10-17 18:37:48 -0300

    adding prod url to allowed origins for CORS

--- src/index.ts
+++ src/index.ts
@@ -12,11 +12,26 @@ import { sequelize } from './sequelize';
 config(); // Load environment variables
 
 const app = express();
+
+const allowedOrigins = ['http://localhost:3000', 'https://the-truth-site.netlify.app'];
+
+// app.use(cors({
+//   origin: 'http://localhost:3000',  
+//   methods: ['GET', 'POST', 'PUT','PATCH', 'DELETE'], 
+//   allowedHeaders: ['Content-Type', 'Authorization'],
+//   credentials: true,  
+// }));
 app.use(cors({
-  origin: 'http://localhost:3000',  
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
   methods: ['GET', 'POST', 'PUT','PATCH', 'DELETE'], 
   allowedHeaders: ['Content-Type', 'Authorization'],
-  credentials: true,  
+  credentials: true, 
 }));
 
 app.use(express.json());  
